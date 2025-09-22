054f3c2278438eb6c5d1889b220de8b802089e3d
learningpath2025 <learningpath2025@outlook.com>
2025-09-22 13:23:12 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment5._2._3
+{
+    internal class Program
+    {
+        static int ln;
+        static void Main(string[] args)
+        {
+            GetInput();
+            PrintNumbers();
+        }
+
+        public static void GetInput()
+        {
+            Console.Write("How many numbers to print: ");
+            ln = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void PrintNumbers()
+        {
+            if (ln > 0)
+            {
+                Console.Write(ln + " ");
+                ln--;
+                PrintNumbers();
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Assignment5.2.3
+
+## Recursion (Reverse)
+
+- Write a program in C# Sharp to print numbers from n to 1 using recursion.
+- Test Data :
+	- How many numbers to print : 10
+	- Expected Output :
+	- 10 9 8 7 6 5 4 3 2 1
